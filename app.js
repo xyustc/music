a32711fae08258d379b6066630cde49a7c9ff7f9
潘星宇 <darkme@mail.dlut.edu.cn>
2021-03-28 10:53:49 +0800

    1.0

---
+++ app.js
@@ -0,0 +1,23 @@
+/*
+ * @Description: 
+ * @Author: XingYu
+ * @Date: 2021-03-22 15:54:36
+ * @LastEditTime: 2021-03-25 08:32:48
+ */
+App({
+
+  onLaunch: function() {
+
+  },
+
+  globalData: {
+    userInfo: null,
+    selectsinger: null,
+    currentIndex: 0,
+    fullScreen: false,
+    songlist: [],
+    playing: false,
+    innerAudioContext: null
+  }
+
+})
